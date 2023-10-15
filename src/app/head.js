89fcc5eddbf5e5dd67bb4555ba915749f93f4adf
Fernando Filho <fernando.fs.filhorn@gmail.com>
2023-10-15 19:19:08 -0300

    Inicio do projeto da pokedex

---
+++ src/app/head.js
@@ -0,0 +1,9 @@
+export default function Head() {
+    return(
+        <>
+            <title>Pokedex</title>
+            <link rel="icon" href="/Pokedex.ico" type="image/x-icon" sizes="16x16"/>
+        </>
+    )
+}
+
---
+++ src/app/layout.js
@@ -0,0 +1,10 @@
+import Head from './head'
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-br">
+      <Head/>
+      <body>{children}</body>
+    </html>
+  )
+}
